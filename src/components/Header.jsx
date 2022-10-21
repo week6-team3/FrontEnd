7aba4c5546056bba9fb5937f32e775187ccf6397
rklskhj <rklskhj2@naver.com>
2022-10-21 23:32:31 +0900

    레이아웃, 헤더 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,75 @@
-import React from 'react'
-import { Button } from '@material-ui/core';
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import styled from 'styled-components';
-import { StyledEngineProvider } from '@mui/styled-engine'
+
 const Header = () => {
     return (
-        <div>
-
-            <span href="/">로고</span>
-            <div>
-
-                <Button color="secondary" href="/users/login/">로그인</Button>
-                <Button color="primary" variant="outlined" href="/users/sign_up/">회원가입</Button>
-
-            </div>
-
-        </div>
-    )
+        <Box sx={{ flexGrow: 1 }}>
+            <BarWrap>
+                <AppBar id="Bar" position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            <span href="/">로고</span>
+                        </Typography>
+                        <Button className='btn' id="btn1" color="inherit" href="/users/login/">로그인</Button>
+                        <Button className='btn' id="btn2" color="inherit" variant="outlined" href="/users/sign_up/">회원가입</Button>
+                    </Toolbar>
+                </AppBar>
+            </BarWrap>
+        </Box>
+    );
 }
 
+
 export default Header
+
+const BarWrap = styled.div`
+    #Bar{
+        background-color: #314057;
+    }
+    
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import { Button } from '@material-ui/core';
+// import styled from 'styled-components';
+// import { StyledEngineProvider } from '@mui/styled-engine'
+// const Header = () => {
+//     return (
+//         <Headerwrap>
+
+//             <span href="/">로고</span>
+//             <BtnBox>
+//                 <StyledEngineProvider injectFirst>
+//                     <Button className='btn' id="btn1" color="inherit" href="/users/login/">로그인</Button>
+//                     <Button className='btn' id="btn2" color="inherit" variant="outlined" href="/users/sign_up/">회원가입</Button>
+//                 </StyledEngineProvider>
+//             </BtnBox>
+//             <div>
+
+//             </div>
+
+//         </Headerwrap>
+//     )
+// }
+
+// export default Header
 
 // const Headerwrap = styled.div`
 //     display: flex;
@@ -26,6 +77,18 @@ export default Header
 //     align-content: center;
 //     justify-content: space-between;
 //     align-items: center;
-//     width: 500rem;
-//     margin: auto 30px;
+//     width: 100rem;
+//     height: 6rem;
+//     margin:10rem auto;
+
+//     background-color: white;
+// `
+
+// const BtnBox = styled.div`
+//     .btn{
+//         font-size: 16px;
+//     }
+//     #btn1{
+//         margin-right: 10px;
+//     }
 // `
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
-
+import Layout from '../components/Layout'
 const Home = () => {
     return (
         <>
-            <Header />
-            <div>홈 가즈아!</div>
+            <Layout>
+                <Header />
+                <div>홈 가즈아!</div>
+            </Layout>
         </>
 
     )
